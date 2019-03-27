975caa6527d8eb53b32fd960a62061e0a3ae436d
zhuxian89 <421690794@qq.com>
2019-03-28 01:21:28 +0800

    add consul-consumer-ribbon

---
+++ springcloud-learn/consul-consumer-ribbon/src/main/java/com/didispace/Application2001.java
@@ -0,0 +1,24 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application2001 {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(Application2001.class).web(true).run(args);
+	}
+
+}
