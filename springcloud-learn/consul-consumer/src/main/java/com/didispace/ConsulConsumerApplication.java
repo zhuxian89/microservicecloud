7c620187a1df449932aaf7ee366763bd9eb59b99
zhuxian89 <40716078+zhuxian89@users.noreply.github.com>
2019-03-27 18:32:58 +0800

    consul version

---
+++ springcloud-learn/consul-consumer/src/main/java/com/didispace/ConsulConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.didispace;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulConsumerApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConsulConsumerApplication.class).web(true).run(args);
+	}
+
+}
